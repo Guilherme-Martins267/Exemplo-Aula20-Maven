682971b4d31b1b034e08451c038a7035b889700d
Guilherme-Martins267 <guihbm.m@gmail.com>
2020-04-18 00:46:16 -0300

    Exemplo SpringBOOT com Maven

---
+++ src/main/java/Martins/Guilherme/ExemploMaven/ExemploMavenApplication.java
@@ -0,0 +1,15 @@
+package Martins.Guilherme.ExemploMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMavenApplication.class, args);
+
+		System.out.println("Opa galera, SpringBOOT com o maven");
+	}
+
+}
